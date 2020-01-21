111d0c1654190bf00a0f8c750a6990c259b394a4
Dzintars Klavins <dzintars.klavins@gmail.com>
2020-01-21 14:57:55 +0200

    fix(uia-pill): add test content

--- packages/uia-pill/src/template.ts
+++ packages/uia-pill/src/template.ts
@@ -1,7 +1,8 @@
-
 import { html, TemplateResult } from 'lit-element';
 import { UiaPill } from './uia-pill';
 
 export default function template(this: UiaPill): TemplateResult {
-  return html``;
+  return html`
+    <h1>Test</h1>
+  `;
 }
